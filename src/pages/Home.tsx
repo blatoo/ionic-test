03097753cfa4209eb5239d8f570bdc6676525a1c
blatoo <connygy@googlemail.com>
2022-05-08 01:15:14 +0200

    test the useIonModul

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,78 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  useIonModal,
+} from "@ionic/react"
+import { useState } from "react"
+import "./Home.css"
+
+const sourceData = [
+  { name: "Tom", age: 16 },
+  { name: "Jerry", age: 12 },
+]
+
+type TItem = {
+  name: string
+  age: number
+}
 
 const Home: React.FC = () => {
+  const [currentLineItem, setCurrentLineItem] = useState<TItem>()
+
+  const handleDismiss = () => {
+    dismiss()
+  }
+
+  const [present, dismiss] = useIonModal(LineItemModal, {
+    LineItem: currentLineItem,
+    onDismiss: handleDismiss,
+  })
+
+  const onEdithandler = (item: TItem) => {
+    setCurrentLineItem(item)
+    present()
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Test useModal Hook</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="ion-padding">
+        {sourceData.map((item) => (
+          <IonRow key={item.name}>
+            <IonCol>name: {item.name}</IonCol>
+            <IonCol>
+              <IonButton onClick={() => onEdithandler(item)}>
+                Click Me
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        ))}
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
+
+type Props = {
+  lineItem: TItem
+  onDismiss: () => void
+}
+const LineItemModal: React.FC<Props> = ({ lineItem, onDismiss }) => {
+  return (
+    <IonContent className="ion-padding">
+      <IonRow>age: {lineItem?.age}</IonRow>
+      <IonButton onClick={onDismiss}>close</IonButton>
+    </IonContent>
+  )
+}
